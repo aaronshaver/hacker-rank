1c24fe47026d317cf7a4677969edeb593620aeb1
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-01-21 21:33:16 -0800

    Two more challenges solved

--- time_conversion.py
+++ time_conversion.py
@@ -5,11 +5,19 @@ import sys
 time = input().strip()
 time_parts = time.split(':')
 
-# a if condition else b
+hour_is_am = True if "AM" in time_parts[2] else False
 
 numeric_hour = int(time_parts[0])
-hour_24_format = "00" if (numeric_hour == 12 and "AM" in time_parts[2]) else str(numeric_hour + 12)
-time_parts[0] = hour_24_format
+if numeric_hour == 12 and hour_is_am:
+    hour_24_format = "00"
+elif numeric_hour < 12 and hour_is_am:
+    hour_24_format = str(numeric_hour).zfill(2) # pad hour with zero if needed
+elif numeric_hour == 12 and not hour_is_am:
+    hour_24_format = numeric_hour
+else:
+    hour_24_format = numeric_hour + 12
+
+time_parts[0] = str(hour_24_format)
 
 time_parts[2] = time_parts[2][:2]
 
