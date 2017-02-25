0da72e3543efe79af26b61c7dc015bc3406143e8
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-02-25 10:32:03 -0800

    circular array rotation solution

--- algorithms-python/circular_array_rotation.py
+++ algorithms-python/circular_array_rotation.py
@@ -2,7 +2,7 @@ num_integers, rotations, num_queries = input().strip().split(' ')
 num_integers, rotations, num_queries = [int(num_integers), int(rotations), int(num_queries)]
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
 
-rotations = rotations % num_integers
+rotations = rotations % num_integers  # if rotations == num_integers, there's no rotations, thus we ignore those
 if num_integers != rotations:
     prepend = a[num_integers-rotations:]
     a = prepend + a
