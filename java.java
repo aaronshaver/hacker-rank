7dbd8e4145c7331f0e500a156f6732c7277057e5
Aaron Shaver <aaronshaverpdx@gmail.com>
2020-12-21 12:03:59 -0800

    initial commits for this new round of interview practice

---
+++ java.java
@@ -0,0 +1,11 @@
+// merely backup/reference of my solutions and for syntax highlighting
+// not intended to be executed from this file!
+
+
+// ---------------------------------------------------------------------------
+// puzzle title: 
+// puzzle link: 
+
+// ---------------------------------------------------------------------------
+
+
---
+++ python.py
@@ -0,0 +1,8 @@
+# merely backup/reference of my solutions and for syntax highlighting
+# not intended to be executed from this file!
+
+## ---------------------------------------------------------------------------
+## puzzle title: 
+## puzzle link: 
+
+## ---------------------------------------------------------------------------
