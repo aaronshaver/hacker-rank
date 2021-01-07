b5ce0b576f57c622f9a3dc8e1c96074e3279cd72
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-07 11:16:47 -0800

    easy one

--- java.java
+++ java.java
@@ -1,5 +1,69 @@
+// ***************************************************************************
 // merely backup/reference of my solutions
 // not intended to be compiled/interpreted/executed from this file!
+// ***************************************************************************
+
+
+// ---------------------------------------------------------------------------
+// puzzle title: Bill Division
+// puzzle link: https://www.hackerrank.com/challenges/bon-appetit/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+        int annaItems = 0;
+        for (int i = 0; i < bill.size(); i++) {
+            if (i != k) {
+                annaItems += bill.get(i);
+            }
+        }
+        
+        final int fairCharge = annaItems / 2;
+        if (b == fairCharge) {
+            System.out.println("Bon Appetit");
+        }
+        else if (b > fairCharge) {
+            System.out.println(b - fairCharge);
+        }
+        else {
+            System.out.println("Undefined behavior");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
+// ---------------------------------------------------------------------------
 
 
 // ---------------------------------------------------------------------------
