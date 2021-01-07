03b960dee8a46f2916f319e32d26601016ea7eab
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-07 11:59:54 -0800

    remove comments in favor of readme.md

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-// merely backup/reference of my solutions
-// not intended to be compiled/interpreted/executed from this file!
 
 
 // ---------------------------------------------------------------------------
--- python.py
+++ python.py
@@ -1,8 +1,8 @@
-# merely backup/reference of my solutions
-# not intended to be compiled/interpreted/executed from this file!
+
 
 ## ---------------------------------------------------------------------------
 ## puzzle title: 
 ## puzzle link: 
 
 ## ---------------------------------------------------------------------------
+
