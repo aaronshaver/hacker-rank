78d0117020fe527a6a4d708f1e64b80acb971201
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-07 11:56:24 -0800

    now featuring SQL! ;-)

---
+++ sql.sql
@@ -0,0 +1,10 @@
+-- merely backup/reference of my solutions
+
+
+-- ---------------------------------------------------------------------------
+-- puzzle title: Revising Aggregations - The Sum Function
+-- puzzle link: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+-- ---------------------------------------------------------------------------
+
