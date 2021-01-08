f7a5ce1ef105193f26ea241676ebb6c03bea709f
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-08 15:03:50 -0800

    more java

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,6 +1,58 @@
 
 
 // ---------------------------------------------------------------------------
+// puzzle title: Drawing Book
+// puzzle link: https://www.hackerrank.com/challenges/drawing-book/problem
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int numPages, int pageTarget) {
+        // my initial instinct was to use a "cursor" of width 2 and brute force it
+        // but then my experience with Project Euler led me to come up with a
+        // math solution
+        final int fromLeft = Math.floorDiv(pageTarget, 2);
+        
+        // for books w/even # pages: create a "virtual last page" to subtract from
+        final int lastPage = numPages % 2 == 0 ? numPages + 1 : numPages;
+        final int fromRight = Math.floorDiv(lastPage - pageTarget, 2);
+        
+        return fromLeft < fromRight ? fromLeft : fromRight;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
 // puzzle title: Sales by Match
 // puzzle link: https://www.hackerrank.com/challenges/sock-merchant/problem
 
