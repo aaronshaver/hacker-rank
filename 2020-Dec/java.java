0eb59a776b8201888b0153f6dc69c2e410dce133
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-13 11:51:43 -0800

    more solution(s)

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,5 +1,64 @@
 
 
+// 7
+// puzzle link: https://www.hackerrank.com/challenges/electronics-shop/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hurdleRace function below.
+    static int hurdleRace(int jumpAbility, int[] heights) {
+        Arrays.sort(heights);
+        final int tallestHurdle = heights[heights.length - 1];
+        final int difference = tallestHurdle - jumpAbility;
+        if (difference <= 0) {
+            return 0;
+        }
+        else {
+            return difference;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] height = new int[n];
+
+        String[] heightItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightItems[i]);
+            height[i] = heightItem;
+        }
+
+        int result = hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
 // ---------------------------------------------------------------------------
 // 6
 // puzzle link: https://www.hackerrank.com/challenges/electronics-shop/problem
