40a43ddb7a33848bd93fa1364076415125f84b89
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-18 14:20:41 -0800

    more Java HackerRank

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,6 +1,77 @@
 
 
 // ---------------------------------------------------------------------------
+// puzzle link: https://www.hackerrank.com/challenges/angry-professor/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the angryProfessor function below.
+    static String angryProfessor(int k, int[] a) {
+        Arrays.sort(a); // FYI, this is broken on Java 8 on HackerRank for some reason? so using their Java 7 option
+        int onTime = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (onTime < k && a[i] > 0) {
+                return "YES";
+            }
+            else if (a[i] <= 0) {
+                onTime += 1;
+                if (onTime >= k) {
+                    return "NO";
+                }
+            }
+        }
+        
+        return "UNKNOWN";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] a = new int[n];
+
+            String[] aItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int aItem = Integer.parseInt(aItems[i]);
+                a[i] = aItem;
+            }
+
+            String result = angryProfessor(k, a);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
 // puzzle link: https://www.hackerrank.com/challenges/utopian-tree/problem
 
 import java.io.*;
