2c52a6c5115c372b765a4a503589708d174e578e
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-18 11:10:11 -0800

    more Java HackerRank

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,6 +1,64 @@
 
 
 // ---------------------------------------------------------------------------
+// puzzle link: https://www.hackerrank.com/challenges/utopian-tree/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) {
+        int height = 1;
+        boolean isSpring = true;
+        
+        for (int i = 0; i < n; i++) {
+            if (isSpring) {
+                height *= 2;
+            }
+            else {
+                height += 1;
+            }
+            
+            isSpring = isSpring == true ? false : true;
+        }
+    
+        return height;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
 // 8
 // puzzle link: https://www.hackerrank.com/challenges/electronics-shop/problem
 import java.io.*;
