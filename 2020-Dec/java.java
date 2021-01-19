35e415f0a4fd9afb0808586acf4bc7b44796c37e
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-19 15:59:22 -0800

    more Java fun

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,6 +1,61 @@
 
 
 // ---------------------------------------------------------------------------
+// puzzle link: https://www.hackerrank.com/challenges/find-digits/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        final String numString = String.valueOf(n);
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < String.valueOf(n).length(); i++) {
+            nums.add(Integer.parseInt(String.valueOf(numString.charAt(i))));
+        }
+        
+        int total = 0;
+        for (Integer num : nums) {
+            if (num == 0) continue;
+            if (n % num == 0) total++; 
+        }
+        
+        return total; 
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
 // puzzle link: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
 
 import java.io.*;
