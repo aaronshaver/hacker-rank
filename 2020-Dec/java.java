eea8911953889c66f109c5ed3f3a5f573605dd1e
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-02-04 16:08:41 -0800

    Java HashMap fun

--- 2020-Dec/java.java
+++ 2020-Dec/java.java
@@ -1,6 +1,70 @@
 
 
 // ---------------------------------------------------------------------------
+// puzzle link: https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : arr) {
+            int count;
+            count = counts.get(num) != null ? counts.get(num) + 1 : 1;
+            counts.put(num, count);
+        }
+        int largestCount = 0;
+        int largestEntry = -1;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > largestCount) {
+                largestCount = entry.getValue();
+                largestEntry = entry.getKey();
+            }
+        }
+        
+        return arr.length - largestCount;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+// ---------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------------------
 // puzzle link: https://www.hackerrank.com/challenges/cut-the-sticks/problem
 // This was a really fun puzzle!!
 
