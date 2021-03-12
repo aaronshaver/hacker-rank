63dd13c141c94ec91ad6a14a01b1004370f8019b
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-03-12 15:25:23 -0800

    SQL fun

--- 2020-Dec/sql.sql
+++ 2020-Dec/sql.sql
@@ -1,6 +1,13 @@
 
 
 -- ---------------------------------------------------------------------------
+-- puzzle link: https://www.hackerrank.com/challenges/weather-observation-station-17/problem 
+
+select round(long_w, 4) from station where lat_n > 38.7780 order by lat_n asc limit 1
+-- ---------------------------------------------------------------------------
+
+
+-- ---------------------------------------------------------------------------
 -- puzzle link: https://www.hackerrank.com/challenges/the-report/problem
 
 select
